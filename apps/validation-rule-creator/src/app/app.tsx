e7e54f10512c589f0d74c60071d894bb9eec6943
rajey <rajeyw@gmail.com>
2023-04-27 23:33:54 +0300

    chore: add initial boilerplate for app

---
+++ apps/validation-rule-creator/src/app/app.tsx
@@ -0,0 +1,33 @@
+import { DataProvider, DataQuery } from '@dhis2/app-runtime';
+import i18n from '@dhis2/d2-i18n';
+import React from 'react';
+
+import styles from './app.module.scss';
+
+const query = {
+  me: {
+    resource: 'me',
+  },
+};
+
+const App = () => (
+  <div className={styles.container}>
+    <DataProvider>
+      <DataQuery query={query}>
+        {({ error, loading, data }) => {
+          const me: any = data?.me;
+          if (error) return <span>ERROR</span>;
+          if (loading) return <span>...</span>;
+          return (
+            <>
+              <h1>{i18n.t('Hello {{name}}', { name: me?.name })}</h1>
+              <h3>{i18n.t('Welcome to DHIS2!')}</h3>
+            </>
+          );
+        }}
+      </DataQuery>
+    </DataProvider>
+  </div>
+);
+
+export default App;
