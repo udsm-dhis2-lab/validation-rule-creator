d4c98e0c0f1fe4c8b689a50728cd3f4522d21f0d
Josephat Mwakyusa <josephatjulius24@gmail.com>
2023-04-29 13:19:53 +0300

    Ref: Remove create test codes

--- apps/validation-rule-creator/src/app/modules/ValidationRuleCreate/containers/Create/Create.test.ts
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import Create from './Create';
-
-describe('ValidationRuleCreate', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<Create />);
-    expect(baseElement).toBeTruthy();
-  });
-});
